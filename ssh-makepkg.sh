3a711904d2ceb6459c2c3a6cc4098ff793c914ba
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-05-16 12:33:25 +0300

    exit removed

--- ssh-makepkg.sh
+++ ssh-makepkg.sh
@@ -30,7 +30,7 @@ function missing_deps {
 }
 
 function deps_build {
-  local miss=$(cower --format='%K %M' -i $1 | cut -f1 -d">")
+  local miss=$(cower --format='%D %K %M' -i $1 | cut -f1 -d">")
   for i in $miss
     do BLD[$dep_num]=$i ; dep_num=$((dep_num+1))
   done
@@ -65,7 +65,7 @@ pkg=${PKG[@]}
 dep=${DEP[@]} 
 bld=${BLD[@]}
 
-function_check_installed=$(type check_installed | grep -v function) ; echo $function_check_installed ; ssh -t $ip $(echo '-p' $port) "eval $function_check_installed" "
+function_check_installed=$(type check_installed | grep -v function) ; ssh -t $ip $(echo '-p' $port) "eval $function_check_installed" "
 export localport=\"$localport\"" "remoteuser=$remoteuser" "pkg=$pkg" "dep=$dep" "bld=$bld" '
 iplocal=$(echo $SSH_CLIENT)' 'EDITOR=/bin/true' '
 PATH="/usr/local/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"' '
@@ -73,41 +73,70 @@ PATH="/usr/local/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr
 while true; do sudo -v; sleep 40; done &
 sudo pacman -Syu
 
-
-if [ ! -z $(echo ${aDEP[@]}) ]
-  then if [ ! -e /tmp/build ] ; then mkdir /tmp/build ; fi ; cd /tmp/build
-  cower -d ${aDEP[@]}
-  for i in $PWD/*
-    do cd $i
-    yes | makepkg -sir 
-  done
-fi 
-
-export PKGDEST="/tmp/scp"
+declare -a old_DEPs
+dep_num=1
 iplocal=$(echo $iplocal | cut -d \  -f 1)
+
+function buildpkg {
+  cower -d $1 ; cd $1 ; yes | makepkg -sri
+  cd /tmp/build ; rm -rf $1
+}
+
+function built {
+  builtat=$(find /tmp/scp/ -name "*.pkg.*" | grep $1)
+  if [[ ! -z $builtat ]]
+    then echo Found previously built packages.
+    export builtat=$builtat
+    else return 0
+  fi
+}
+
 if [ ! -e /tmp/scp ] ; then mkdir /tmp/scp ; fi
 if [ ! -e /tmp/build ] ; then mkdir /tmp/build ; fi ; cd /tmp/build
-cower -d $(echo $pkg)
 
-for i in $PWD/*
-  do cd $i
-  if [[ ! -z $(find /tmp/scp/ -name "*.pkg.*" | grep $i) ]]
-    then echo Found previously built packages.
-    else yes | makepkg -sr 
+for i in $(echo $blk) ; do
+  if [ -z $(echo $pkg | grep $i) ] || [ -z $(echo $dep | grep $i) ]
+    then check_installed $i ;  if [ $? = 1 ]
+      then built $i ; if [ ! $? = 0 ]
+        then sudo pacman -U $builtat
+        else buildpkg $i
+      fi
+      old_DEPs[$dep_num]=$i ; dep_num=$((dep_num+1))
+    fi
   fi
 done
 
-if [ ! -z ${aDEP[@]} ] ; then yes | sudo pacman -Rc ${aDEP[@]} ; fi
+export PKGDEST="/tmp/scp"
+
+for i in $(echo $dep) ; do
+  built $i ; if [ ! $? = 0 ]
+    then sudo pacman -U $builtat ; old_DEPs[$dep_num]=$i ; dep_num=$((dep_num+1))
+    else buildpkg $i
+  fi
+done
+
+for i in $(echo $pkg) ; do
+  if [ -z $(echo $dep | grep $i) ]
+    then built $i ; if [ ! $? = 0 ]
+      then sudo pacman -U $builtat
+      else buildpkg $i
+    fi
+    else echo $i has already been built
+  fi
+done
+
+if [ ! -z ${old_DEPs[@]} ] ; then 
+  yes | sudo pacman -Rc ${old_DEPs[@]} ; fi
 
 rm -rf /tmp/build/
 echo Trying to scp
 scp $localport /tmp/scp/* $remoteuser@$iplocal:/tmp/scp-receive ; if [ ! $? = 0 ] 
   then rm -rf /tmp/scp/* 
-  else Something went wrong with scp from the remote machine's' side. Are you behind nat? Built packages are in /tmp/scp .
+  else Something went wrong with scp from the remote machine side. Are you behind nat\? Built packages are in /tmp/scp .
 fi
 sudo -k
+killall -9 makepkg
 '
-exit
 
 if [ -z $(ls /tmp/scp-receive/) ]
   then echo 'This should probably work... maybe'
